b20b24aa4fed11fd9c66bcbfa00a21345390f09d
Florian <45694132+flo-bit@users.noreply.github.com>
2026-01-23 22:52:52 +0100

    small fix for section titles

--- src/lib/cards/SectionCard/EditingSectionCard.svelte
+++ src/lib/cards/SectionCard/EditingSectionCard.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class="line-clamp-1 inline-flex h-full w-full items-center rounded-md p-1 px-2 text-2xl font-semibold"
+	class="line-clamp-1 inline-flex h-full w-full items-end rounded-md p-1 px-2 text-2xl font-semibold"
 >
 	<PlainTextEditor bind:item key="text" class="line-clamp-1 w-full" />
 </div>
--- src/lib/cards/SectionCard/SectionCard.svelte
+++ src/lib/cards/SectionCard/SectionCard.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div
-	class="line-clamp-1 inline-flex h-full w-full items-center rounded-md p-1 px-2 text-2xl font-semibold"
+	class="line-clamp-1 inline-flex h-full w-full items-end rounded-md p-1 px-2 text-2xl font-semibold"
 >
 	{item.cardData.text}
 </div>
